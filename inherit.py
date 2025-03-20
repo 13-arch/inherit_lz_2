f10c1289b28d7b2d24a7ec79608f31f7b0c040ef
13-arch <Iron_Mike@tutamail.com>
2025-03-20 17:42:26 +0300

    20.03.2025

--- inherit.py
+++ inherit.py
@@ -1,16 +1,38 @@
 #13-arch
 
 
+import hashlib
+
 class Login:
-    def __init__(self,name,surname,login):
-        self.n=name
-        self.sn=surname
-        self.l=login
-    
+    employees = {
+        'senya': 'arseniy okun',
+        'bobr': 'ilua bobro',
+        'zoro': 'anton zorecki'
+    }
+
+    def __init__(self, name, surname, login):
+        self.name = name
+        self.surname = surname
+        self.login = login
 
     def info(self):
-        print(f'ИМЯ:{self.n},ФАМИЛИЯ:{self.sn},ЛОГИН:{self.l}')
-
-
+        return f'Name: {self.name}, Surname: {self.surname}, Login: {self.login}'
+    
     def is_employee(self):
+        return self.login in self.employees
+
+
+class Authorization(Login):
+    def __init__(self, name, surname, login, password):
+        super().__init__(name, surname, login)
+        self.password = password
+        self.is_hashed = False
+
+    def info(self):
+        return f'{super().info()}, Password: {"hashed" if self.is_hashed else self.password}'
+
+    def hashing_pass(self):
+        if not self.is_hashed:
+            self.password = hashlib.sha256(self.password.encode()).hexdigest()
+            self.is_hashed = True
         
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from inherit import Login
+from inherit import Authorization
 
 
 def main(): #создаем функцию
-    first=Login('Andrey','korovai','andrkorov')
-    first.info()
+    user = Authorization('arseniy', 'okun', 'senya', 'mypassword')
+    print(user.info())
+    print("Is employee:", user.is_employee())
+    user.hashing_pass()
+    print(user.info())
 if __name__ == '__main__': #проверяем на прямой запуск
     main() #запускаем функцию
